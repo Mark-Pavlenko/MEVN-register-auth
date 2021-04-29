952c8f39a41408fbde3b7141491346d34b821448
Marko Pavlenko <mark.pavlenko@megadevllc.com>
2021-04-29 11:02:55 +0300

    Optimized views files

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: "mongodb+srv://marik8998:marik8998@movie-db-api.obzjk.mongodb.net/database?retryWrites=true&w=majority",
+    database: "mongodb+srv://marik8998:marik8998@movie-db-api.obzjk.mongodb.net/movie-db-api?retryWrites=true&w=majority",
     secret: "password"
 };
